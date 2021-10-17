885cf2cef3e1f0fb6a64c40cc41f9ac40a4b6f38
Azraf C <c.azraf@gmail.com>
2021-10-17 15:17:06 +0600

    added woocommerce category and tag remove options

--- wc_utils.php
+++ wc_utils.php
@@ -34,10 +34,50 @@ if(isset($_POST['delete_wc_products'])){
     echo "You dont have sufficient privilege to perform this action!";
     } // current_user_can method END
 }
+
+if(isset($_POST['delete_wc_product_cats'])){
+    if ( current_user_can( 'manage_options' ) ) {
+        if ( check_admin_referer( 'mws_wc_delproductcats' ) ) {
+            global $wpdb;
+            $sql = "DELETE a,c FROM ".$wpdb->prefix."terms AS a
+            LEFT JOIN ".$wpdb->prefix."term_taxonomy AS c ON a.term_id = c.term_id
+            LEFT JOIN ".$wpdb->prefix."term_relationships AS b ON b.term_taxonomy_id = c.term_taxonomy_id
+            WHERE c.taxonomy = 'product_cat'";
+            try{
+                $wpdb->query($sql);
+                echo "<br /><h3>Success! All product categories removed!</h3>";
+            }catch(Exception $e){
+                echo $e->getMessage();
+            }
+        }
+    } else {
+    echo "You dont have sufficient privilege to perform this action!";
+    } // current_user_can method END
+}
+
+if(isset($_POST['delete_wc_product_tags'])){
+    if ( current_user_can( 'manage_options' ) ) {
+        if ( check_admin_referer( 'mws_wc_delproduct_tags' ) ) {
+            global $wpdb;
+            $sql = "DELETE a,c FROM ".$wpdb->prefix."terms AS a 
+            LEFT JOIN ".$wpdb->prefix."term_taxonomy AS c ON a.term_id = c.term_id
+            LEFT JOIN ".$wpdb->prefix."term_relationships AS b ON b.term_taxonomy_id = c.term_taxonomy_id
+            WHERE c.taxonomy = 'product_tag'";
+            try{
+                $wpdb->query($sql);
+                echo "<br /><h3>Success! All product categories removed!</h3>";
+            }catch(Exception $e){
+                echo $e->getMessage();
+            }
+        }
+    } else {
+    echo "You dont have sufficient privilege to perform this action!";
+    } // current_user_can method END
+}
 ?>
 <div>
     <h2>Tasks:</h2>
-    <div class="">
+    <div class="wc-action">
     <?php
     if ( current_user_can( 'manage_options' ) ) {
     ?>
@@ -45,6 +85,32 @@ if(isset($_POST['delete_wc_products'])){
                 Delete All woocommerce products &nbsp; <input type="hidden" name="delete_wc_products" />
                 <?php wp_nonce_field( 'mws_wc_delproducts');?>
             <?php  submit_button('Delete Products'); ?>
+        </form>
+        <?php
+    }
+    ?>
+    </div>
+    <div class="wc-action">
+    <?php
+    if ( current_user_can( 'manage_options' ) ) {
+    ?>
+        <form method="post" action="" onsubmit="return confirm('Do you really want to DELETE all Woocommerce Product Categories?');">
+                Delete All woocommerce product categories &nbsp; <input type="hidden" name="delete_wc_product_cats" />
+                <?php wp_nonce_field( 'mws_wc_delproductcats');?>
+            <?php  submit_button('Delete Categories'); ?>
+        </form>
+        <?php
+    }
+    ?>
+    </div>
+    <div class="wc-action">
+    <?php
+    if ( current_user_can( 'manage_options' ) ) {
+    ?>
+        <form method="post" action="" onsubmit="return confirm('Do you really want to DELETE all Woocommerce Product Tags?');">
+                Delete All woocommerce product tags &nbsp; <input type="hidden" name="delete_wc_product_tags" />
+                <?php wp_nonce_field( 'mws_wc_delproduct_tags');?>
+            <?php  submit_button('Delete Products Tags'); ?>
         </form>
         <?php
     }
